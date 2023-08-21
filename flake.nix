d9b4e23a7506a1465c1a26cdb3f51e9b313862d3
Garrett George <garrett.michael.george@gmail.com>
2023-08-21 16:58:15 -0500

    feat: add pytests to python script builds

--- flake.nix
+++ flake.nix
@@ -17,21 +17,7 @@
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
         let
-          mkShellScript = name: ext: runtimeInputs: pkgs.writeShellApplication {
-            inherit name runtimeInputs;
-            text =
-              let maybeExt = if pkgs.lib.isString (ext) then ".${ext}" else "";
-              in builtins.readFile ./scripts/${name}/${name}${maybeExt};
-          };
-
-          mkShellScript2 = { name, runtimeInputs, ext ? null }: pkgs.writeShellApplication {
-            inherit name runtimeInputs;
-            text =
-              let maybeExt = if pkgs.lib.isString (ext) then ".${ext}" else "";
-              in builtins.readFile ./scripts/${name}/${name}${maybeExt};
-          };
-
-          lib = import lib/scripts.nix;
+          lib = import lib/scripts.nix { inherit pkgs; };
         in
         {
           # Per-system attributes can be defined here. The self' and inputs'
@@ -49,21 +35,12 @@
               text = builtins.readFile ./scripts/my-first-script/my-first-script.sh;
             };
 
-            # Short form
-            script-with-deps = mkShellScript "script-with-deps" "sh" [ pkgs.cowsay ];
-
-            # Medium form; not much shorter than writeShellApplication, but
-            # enforces the repo directory convention (scripts/${pkgname}.${ext})
-            script-with-deps2 = mkShellScript2 {
+            script-with-deps = lib.mkShellScript {
               name = "script-with-deps";
               runtimeInputs = [ pkgs.cowsay ];
-              ext = "sh";
             };
 
-            print-date = lib.mkPythonPoetryScript {
-              name = "print-date";
-              inherit pkgs;
-            };
+            print-date = lib.mkPythonPoetryScript { name = "print-date"; };
           };
 
           checks = config.packages;
--- lib/scripts.nix
+++ lib/scripts.nix
@@ -1,6 +1,36 @@
+{ pkgs }:
+
 {
-  mkPythonPoetryScript = { name, pkgs }: pkgs.poetry2nix.mkPoetryApplication {
-    meta.mainProgram = name;
-    projectDir = ../scripts/${name};
+  /* Create a derivation for a Python script managed by Poetry in scripts/
+
+    Enforces the directory convention where a Poetry project is supposed to
+    exist in scripts/${scriptName}/ and export a script with the same name
+    under [tool.poetry.scripts].
+  */
+  mkPythonPoetryScript = { name }:
+    let python = pkgs.python3;
+    in
+    pkgs.poetry2nix.mkPoetryApplication {
+      inherit python;
+      projectDir = ../scripts/${name};
+      doCheck = true;
+      # nativeCheckInputs = [ pkgs.python3Packages.pytestCheckHook ];
+
+      checkPhase = ''
+        runHook preCheck
+        pytest
+        runHook postCheck
+      '';
+
+      meta.mainProgram = name;
+    };
+
+  /* Create a derivation for a shell script in scripts/
+
+    Enforces the directory convention scripts/${scriptName}/${scriptName}.sh.
+  */
+  mkShellScript = { name, runtimeInputs }: pkgs.writeShellApplication {
+    inherit name runtimeInputs;
+    text = builtins.readFile ../scripts/${name}/${name}.sh;
   };
 }
